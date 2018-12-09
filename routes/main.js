642e21be4b4aa896746f8b05e7d618ab490a84be
Sorin Cătană <scatana@users.noreply.github.com>
2018-12-08 19:26:11 -0500

    Use locale middleware

--- routes/main.js
+++ routes/main.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Save the requested locale into a cookie if it differs from the current locale;
-// the cookie should expire in one year; this happens for all routes that this
-// router handles
-router.use((req, res, next) => {
-  const requestedLocale = req.baseUrl.split('/')[1];
-  const currentLocale = req.getLocale();
-
-  if (requestedLocale !== currentLocale) {
-    res.setLocale(requestedLocale);
-    res.cookie('locale', requestedLocale, {
-      expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000) // one year
-    });
-  }
-
-  next();
-});
+router.use(require('./middleware/locale'));
 
 router.get('/', (req, res) => res.render('landing', {
   title: `${res.__('brand.name')} - ${res.__('brand.description')}, ${res.__('contact.city')}`,
