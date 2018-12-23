84d3ccddb156aecd8209e6f9f63e5165e127cc12
Sorin Cătană <scatana@users.noreply.github.com>
2018-12-23 11:09:04 -0500

    Update navbar when navigation panel gets open / closed

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,19 @@
 $(function () {
   'use strict'
 
-  $('[data-toggle="offcanvas"]').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open')
-  })
-
   let offsetY = 0;
-  let $navbar = $(".navbar");
+  let $navbar = $('.navbar');
+  let $navpanel = $('.offcanvas-collapse');
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $navpanel.toggleClass('open');
+    updateNavbar();
+  })
 
   function updateNavbar() {
     offsetY = $(window).scrollTop();
 
-    if (offsetY > 0) {
+    if (offsetY > 0 || $navpanel.hasClass('open')) {
       $navbar.css({'background':'#fff', 'box-shadow':'0 0 5px #555'});
       $navbar.removeClass('navbar-dark').addClass('navbar-light');
     } else {
