7436dea3d768d748e3125df493d024cbccea753d
Sorin Cătană <scatana@users.noreply.github.com>
2018-12-23 00:56:23 -0500

    Rates tweaks, navbar shadow on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,17 @@ $(function () {
   $('[data-toggle="offcanvas"]').on('click', function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
+
+  let offsetY = 0;
+  let $navbar = $(".navbar");
+
+  $(window).scroll(function(){
+    offsetY = $(window).scrollTop();
+
+    if( offsetY > 0 ){
+      $navbar.css({'box-shadow':'0 0 5px #ccc'});
+    } else {
+      $navbar.css({'box-shadow':'none'});
+    }
+  });
 });
