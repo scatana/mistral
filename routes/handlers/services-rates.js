05b8c0061b2c8f9d4eb98779e7a413d39560a349
Sorin Cătană <scatana@users.noreply.github.com>
2018-12-26 17:41:48 -0500

    Localized routes tweaks

--- routes/handlers/services-rates.js
+++ routes/handlers/services-rates.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => res.render('services-rates', {
-  title: `${res.__('brand.name')} - ${res.__('content.services-rates')}`,
+  title: `${res.__('brand.name')} - ${res.__('routes.services-rates.title')}`,
   description: `${res.__('routes.services-rates.description')}`
 });
