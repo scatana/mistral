68c10a656789a04d7908728fa578b74a7809c887
Sorin Cătană <scatana@users.noreply.github.com>
2019-01-04 21:57:25 -0500

    Sitemap updates

--- routes/handlers/sitemap.js
+++ routes/handlers/sitemap.js
@@ -1,51 +1,39 @@
 module.exports = (req, res) => {
+  const base = "https://www.valentinacatana.com";
+  const locales = res.getLocales();
+  const routes = res.__('routes');
+
+  let output = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  output += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n';
+
+  locales.forEach(locale => {
+    output += '<url>\n';
+    output += `<loc>${base}/${locale}</loc>\n`;
+
+    locales.forEach(loc => {
+      output += `<xhtml:link rel="alternate" hreflang="${loc}" href="${base}/${loc}"/>\n`;
+    });
+
+    output += '</url>\n';
+  });
+
+  locales.forEach(locale => {
+    for (route in routes) {
+      if (routes[route]._type === 'get') {
+        output += '<url>\n';
+        output += `<loc>${base}/${locale}/${res.__({ phrase: 'routes.' + route + '.path', locale })}</loc>\n`;
+
+        locales.forEach(loc => {
+          output += `<xhtml:link rel="alternate" hreflang="${loc}" href="${base}/${loc}/${res.__({ phrase: 'routes.' + route + '.path', locale: loc })}"/>\n`;
+        });
+
+        output += '</url>\n';
+      }
+    }
+  });
+
+  output += '</urlset>\n';
+
   res.set('Content-Type', 'application/xml');
-  res.send(`<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-  <url>
-    <loc>https://www.valentinacatana.com/en/</loc>
-    <xhtml:link
-      rel="alternate"
-      hreflang="fr"
-      href="https://www.valentinacatana.com/fr/"/>
-    <xhtml:link
-      rel="alternate"
-      hreflang="ro"
-      href="https://www.valentinacatana.com/ro/"/>
-    <xhtml:link
-      rel="alternate"
-      hreflang="en"
-      href="https://www.valentinacatana.com/en/"/>
-  </url>
-  <url>
-    <loc>https://www.valentinacatana.com/fr/</loc>
-    <xhtml:link
-      rel="alternate"
-      hreflang="en"
-      href="https://www.valentinacatana.com/en/"/>
-    <xhtml:link
-      rel="alternate"
-      hreflang="ro"
-      href="https://www.valentinacatana.com/ro/"/>
-    <xhtml:link
-      rel="alternate"
-      hreflang="fr"
-      href="https://www.valentinacatana.com/fr/"/>
-  </url>
-  <url>
-    <loc>https://www.valentinacatana.com/ro/</loc>
-    <xhtml:link
-      rel="alternate"
-      hreflang="en"
-      href="https://www.valentinacatana.com/en/"/>
-    <xhtml:link
-      rel="alternate"
-      hreflang="fr"
-      href="https://www.valentinacatana.com/fr/"/>
-    <xhtml:link
-      rel="alternate"
-      hreflang="ro"
-      href="https://www.valentinacatana.com/ro/"/>
-  </url>
-</urlset>`);
+  res.send(output);
 };
